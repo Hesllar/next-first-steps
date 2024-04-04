de2bd0991d33dfc974290aa83cbaf51edadb62c0
Hesllar Henry <hesllar.linzmayer@gmail.com>
2024-04-03 23:25:05 -0300

    First commit

---
+++ app/(general)/contact/page.tsx
@@ -0,0 +1,16 @@
+import { Metadata } from "next";
+
+
+export const metadata:Metadata = {
+ title: 'SEO Title Contact',
+ description: 'SEO Description Contact',
+};
+
+export default function ContactPage(){
+    return (
+      <>
+          <span className="text-7xl">Contact Page</span>
+      </>
+    )
+  }
+  
